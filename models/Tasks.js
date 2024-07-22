3ecd457ca26f0cc68ae17ab65ef0a8ecf9f83017
Gulab Singh SIkarwar <94665834+GulabSinghSikarwar@users.noreply.github.com>
2024-07-22 19:14:36 +0530

    Task API

---
+++ models/Tasks.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['Pending', 'In Process', 'Completed'],
+        required: true
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Task = mongoose.model('Task', taskSchema);
+module.exports = Task;
